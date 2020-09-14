8b7f0a57b1a81899dedd2c3a84e17b103c74d822
ppearce05 <ppearce05@Josephs-Mac.local>
2020-09-14 09:57:46 -0700

    add Help Queue project and meaninful fail test

---
+++ help-queue/src/_tests_/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,9 @@
+import ticketLIstReducer from '../../reducers/ticket-list-reducer';
+
+describe('ticketListReducer', () => {
+  
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+
+});
