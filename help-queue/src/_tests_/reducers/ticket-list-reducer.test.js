eed7a059bfddee8cb995809336a4ed6184c2438f
Joseph Pearce <joseph.pearce05@gmail.com>
2020-09-15 10:31:41 -0700

    add three tests - successful

--- help-queue/src/_tests_/reducers/ticket-list-reducer.test.js
+++ help-queue/src/_tests_/reducers/ticket-list-reducer.test.js
@@ -3,23 +3,31 @@ import ticketListReducer from '../../reducers/ticket-list-reducer';
 describe('ticketListReducer', () => {
 
   let action;
+
+  const currentState = {
+    1: {names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1 },
+    2: {names: 'Jasmine and Justine',
+    location: '2a',
+    issue: 'Reducer has side effects.',
+    id: 2 }
+  }
+
   const ticketData = {
     names: 'Ryan & Aimen',
     location: '4b',
     issue: 'Redux action is not working correctly.',
     id: 1
-  },
+  };
 
-  // Test 1
-
-  test('Should return default state if there is no action type passed into the reducer', () => {
+  test('Should return default state if no action type is recognized', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
-  // Test 2
-
   test('Should successfully add new ticket data to masterTicketList', () => {
-    const { names, location, issue, id} = ticketData;
+    const { names, location, issue, id } = ticketData;
     action = {
       type: 'ADD_TICKET',
       names: names,
@@ -27,16 +35,27 @@ describe('ticketListReducer', () => {
       issue: issue,
       id: id
     };
-  
-      expect(ticketListReducer({}, action)).toEqual({
-        [id]: {
-          names: names,
-          location: location,
-          issue: issue,
-          id: id
-        }
-      });
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
   });
 
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2 }
+    });
+  });
 
 });
